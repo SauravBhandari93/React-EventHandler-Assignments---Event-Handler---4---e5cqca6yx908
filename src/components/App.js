60ae92c61b8185dd12bc0a8818c3f76294f905ed
SauravBhandari93 <83832259+SauravBhandari93@users.noreply.github.com>
2022-03-26 18:12:49 +0530

    first commint

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useState } from "react";
+import "../styles/App.css";
 
-  const handleDoubleClick = (event) =>{
-  
-  }
+let timer = undefined;
+
+const App = () => {
+  const [displayText, setDisplayText] = useState("");
+  const [prevent, setPrevent] = useState(false);
+
+  const handlSingleClick = (event) => {
+    timer = setTimeout(() => {
+      if (!prevent) {
+        console.log("I was not double clicked");
+      }
+    }, 200);
+  };
+
+  const handleDoubleClick = (event) => {
+    clearTimeout(timer);
+    setPrevent(true);
+
+    console.log("I was double clicked");
+    setTimeout(() => {
+      setPrevent(false);
+    }, 200);
+  };
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button
+        id="dblclick-btn"
+        onDoubleClick={handleDoubleClick}
+        onClick={handlSingleClick}
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
